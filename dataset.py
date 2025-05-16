44b14090e696a84a48bd8a23fb61dbff35dffaf9
Abraham Kuruvila <abrahamkuruvila@proton.me>
2025-05-16 19:13:02 +0400

    some last min changes

--- dataset.py
+++ dataset.py
@@ -3,112 +3,103 @@ import cv2
 import tensorflow as tf
 import json
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python.vision.hand_landmarker import HandLandmark, HandLandmarkerOptions
 import numpy as np
-from tensorflow.python.keras.engine.training_utils_v1 import validate_dataset_input
 
-from sort import video_id
-
-#loading files
+# Load files
 mediapipe_data_path = '../Dataset/hand_tracking.task'
-
 with open('../Dataset/WSASL/Index.json', 'r') as f:
     data = json.load(f)
 
-all_landmark = list()
-array_dict = {}
-np.set_printoptions(precision=4)
+# Build class-to-index mapping
+all_glosses = sorted(set(x['gloss'] for x in data))
+gloss_to_id = {gloss: idx for idx, gloss in enumerate(all_glosses)}
 
-
-#loading classes
+# Setup MediaPipe
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=mediapipe_data_path),
+    running_mode=VisionRunningMode.VIDEO
+)
 
-#config the landmarker
-options = HandLandmarkerOptions(base_options = BaseOptions(model_asset_path = mediapipe_data_path), running_mode = VisionRunningMode.VIDEO)
 def video_generator():
-    video_number = 1
     global_framecount = 0
-    with HandLandmarker.create_from_options(options) as landmarker: #initializes mp handlandmarker
-        #frame splicing
+    video_number = 1
+    with HandLandmarker.create_from_options(options) as landmarker:
         for x in data:
-            instance = x['instances']
-            for y in instance:
-                if y['split'] == 'train': # caused by the splitting of video by my stupid ass for the tfrecords
-                    video_path ='../WSASL/Dataset/train'
-                elif y['split'] == 'test':
-                    video_path = '../WSASL/Dataset/test'
-                elif y['split'] == 'val':
-                    video_path = '../WSASL/Dataset/val'
+            for y in x['instances']:
+                video_path = f"../WSASL/Dataset/{y['split']}"
                 video_id = y['video_id']
-                all_landmark.clear()
                 vid = cv2.VideoCapture(f'{video_path}/{video_id}.mp4')
                 fps = vid.get(cv2.CAP_PROP_FPS)
+                frames = []
+
                 while True:
-                    ret, frame = vid.read() #honestly have no clue why ret still exists but it don't work without it so :Shrug:
+                    ret, frame = vid.read()
                     if not ret:
                         break
-                    else:
-                        timestamp = int(1000*global_framecount/fps) #timestamp for mp(god i hated this.. ts took forever to get working)
-                        frame= cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #converting to Rgb just incase coz mp is a picky eater :/
-                        mp_image = mp.Image(image_format = mp.ImageFormat.SRGB, data = frame) #makes it something mp can process
-                        del frame #we mark frame to be deleted from memory coz we aint needing it anymore
-                        result = landmarker.detect_for_video(mp_image, timestamp)#we mark where the fingers are
-                        del mp_image
-                        global_framecount+=1#increase frame count for mp
-                        if result.hand_landmarks: #checking if result has any data (otherwise it'll throw a fit)
-                            all_landmark.append([[lm.x, lm.y, lm.z] for lm in result.hand_landmarks[0]])#stores the result for this frame
-                            del result#bye bye result hello memory
-                vid.release()# the video is a free man/woman again
-                landmark_arr = np.array(all_landmark) # appends all the frames into one ginormous array.. hmm wonder if it is taller than a trex
-                sample = {
+                    timestamp = int(1000 * global_framecount / fps)
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+                    result = landmarker.detect_for_video(mp_image, timestamp)
+                    global_framecount += 1
+                    if result.hand_landmarks:
+                        frames.append([[lm.x, lm.y, lm.z] for lm in result.hand_landmarks[0]])
+                    if len(frames) >= 160:
+                        break
+
+                vid.release()
+
+                # Pad or trim to 160
+                if len(frames) < 160:
+                    pad_len = 160 - len(frames)
+                    frames.extend([[[0.0, 0.0, 0.0]] * 21] * pad_len)
+                else:
+                    frames = frames[:160]
+
+                landmark_arr = np.array(frames, dtype=np.float32)
+
+                yield {
                     'video_id': tf.constant(video_id),
-                    'landmarks': tf.constant(landmark_arr),  # converting data to tensors.. now we gotta ship em off for processing
+                    'landmarks': tf.constant(landmark_arr),
                     'shape': tf.constant(landmark_arr.shape),
                     'split': tf.constant(y['split']),
-                    'gloss': tf.constant(x['gloss'])
+                    'gloss': tf.constant(gloss_to_id[x['gloss']])
                 }
-                yield sample # officially only now is this thing a generator to tf
-                del landmark_arr
-                print(f'NO of videos done: {video_number}. {video_id} successfully written :thumbs_up:')
-                video_number +=1
+
+                print(f'NO of videos done: {video_number}. {video_id} successfully written.')
+                video_number += 1
                 gc.collect()
 
-
-dataset = tf.data.Dataset.from_generator(  #hocus pocus I understand somewhat
+# TF Dataset
+dataset = tf.data.Dataset.from_generator(
     video_generator,
-    output_signature = {
-        'video_id': tf.TensorSpec(shape = (), dtype = tf.string),
-        'landmarks': tf.TensorSpec(shape = (None, 21, 3), dtype = tf.float32),
-        'shape': tf.TensorSpec(shape = (3,), dtype = tf.int32),
-        'split': tf.TensorSpec(shape = (), dtype = tf.string),
-        'gloss': tf.TensorSpec(shape=(), dtype=tf.string)
+    output_signature={
+        'video_id': tf.TensorSpec(shape=(), dtype=tf.string),
+        'landmarks': tf.TensorSpec(shape=(160, 21, 3), dtype=tf.float32),
+        'shape': tf.TensorSpec(shape=(3,), dtype=tf.int32),
+        'split': tf.TensorSpec(shape=(), dtype=tf.string),
+        'gloss': tf.TensorSpec(shape=(), dtype=tf.int32)
     }
 )
 
-# from here on the hocus pocus is barely understood by me. Except these 5 more lines..
-training_dataset = dataset.filter(lambda x: x['split'] == 'train').shuffle(buffer_size = 100).repeat(10)
-val_dataset = dataset.filter(lambda x: x['split'] == 'test').repeat(10)
-test_dataset = dataset.filter(lambda x :x['split'] == 'test').repeat(10)
-del dataset
-gc.collect()
+training_dataset = dataset.filter(lambda x: x['split'] == 'train').shuffle(100).repeat(10).batch(1)
+val_dataset = dataset.filter(lambda x: x['split'] == 'test').repeat(10).batch(1)
+test_dataset = dataset.filter(lambda x: x['split'] == 'test').repeat(10).batch(1)
 
+# Model
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(None, 21, 3)),
+    tf.keras.layers.Flatten(input_shape=(160, 21, 3)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
+    tf.keras.layers.Dense(len(gloss_to_id))  # num classes
 ])
-model.compile(optimizer = 'adam',
+model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics = ['accuracy'])
+              metrics=['accuracy'])
 
-model.fit(
-    training_dataset,
-    validation_data = val_dataset,
-    epochs = 10,
-)
-test_loss, test_acc = model.evaluate(test_dataset, verbose = 2)
-print('Test Accuracy =',test_acc)
-#if your reading this email me at abrahamkuruvila2008@proton.me with your favorite song right now.... pwease?
+# Training
+model.fit(training_dataset, validation_data=val_dataset, epochs=10)
+test_loss, test_acc = model.evaluate(test_dataset, verbose=2)
+print('Test Accuracy =', test_acc)
